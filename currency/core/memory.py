a99bf6c1857d940ad39f4b4027b380c6bfdfe812
Nikita Pinegin <nobodyni@icloud.com>
2025-04-24 19:30:14 +0300

    Создан файл memory.py
    * Реализован класс MemoryCurrency, наследующий AbstractCurrency:
    	* добавлен метод all, возвращает копию текущих данных о валюте
    	* добавлена возможность инициализации класса как с начальными
    	  значениями, так и с дефолтными для каждой валюты

---
+++ currency/core/memory.py
@@ -0,0 +1,21 @@
+from .abstract import AbstractCurrency
+
+
+class MemoryCurrency(AbstractCurrency):
+    def __init__(self, initial=None):
+        self._currencies = initial or {"USD": 0.0, "RUB": 0.0, "EUR": 0.0}
+
+    def get(self, currency: str) -> float:
+        return self._currencies.get(currency.upper(), 0.0)
+
+    def set(self, currency: str, value: float) -> None:
+        self._currencies[currency.upper()] = value
+
+    def modify(self, currency: str, value: float) -> None:
+        self._currencies[currency.upper()] += value
+
+    def all(self):
+        return self._currencies.copy()
+
+
+currencies = MemoryCurrency()
